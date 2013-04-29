3db764aba11a30a1ade585f0f321fa782606fcef
Yuta Okano <yuta.okano@gmail.com>
2013-04-29 22:16:27 +0900

    ログインフォーム

--- mucha.js
+++ mucha.js
@@ -6,7 +6,7 @@
 enchant();
 
 var game;
-window.addEventListener('load', function() {
+$(function() {
 	game = new Game();
 	game.start();
 });
@@ -35,6 +35,8 @@ var Game = Class.create(Core, {
 	
 	/**
 	 * ゲーム開始時の処理
+	 * @method
+	 * @memberof Game
 	 */
 	onload: function() {
 		// 背景
@@ -48,9 +50,67 @@ var Game = Class.create(Core, {
 		var backlog = new Backlog({
 			url: 'https://okano.backlog.jp/XML-RPC',
 			id: 'okano',
-			pass: 'vtyfu7slg'
+			pass: 'vtyfu7slg',
+			method: 'get_projects'
 		});
-		backlog.toBinary('mynameisokano');
+		
+		var loginForm = new LoginForm();
+	}
+});
+
+/**
+ * ログインフォームクラス
+ * @class
+ * @member {DOM} _div ログインフォームのdiv
+ */
+var LoginForm = Class.create({
+	/**
+	 * コンストラクタ
+	 * @method
+	 * @memberof LoginForm
+	 */
+	initialize: function() {
+		var submit = $('#submit');
+		var space = $('#space');
+		var id = $('#id');
+		var pass = $('#pass');
+		
+		var self = this;
+		submit.click(function() {
+			self._login(space.val(), id.val(), pass.val());
+		});
+	},
+	
+	/**
+	 * ログインする
+	 * @method
+	 * @memberof LoginForm
+	 * @param {String} space Backlogスペース名
+	 * @param {String} id ログインID
+	 * @param {String} pass パスワード
+	 */
+	_login: function(space, id, pass) {
+		if(space == '' || id == '' || pass == '') {
+			alert('入力されていない項目があります');
+			return;
+		}
+		
+		console.log(navigator.userAgent);
+		$.ajax('/backlog', {
+			data: {
+				space: space,
+				id: id,
+				pass: pass,
+				method: 'get_projects',
+				agent: navigator.userAgent
+			},
+			error: function() {
+				console.log('error');
+			},
+			success: function() {
+				console.log('success');
+			}
+		});
 	}
 });
 
@@ -76,60 +136,9 @@ var Backlog = Class.create({
 		this._url = config.url;
 		this._id = config.id;
 		this._pass = config.pass;
+		
 	},
-	
-	/**
-	 * 文字列をバイナリに変換する
-	 * @method
-	 * @memberof Backlog
-	 * @param {String} str 文字列
-	 * @returns {String} Base64 文字列
-	 */
-	encodeBase64: function(str) {
-	
-		// バイト配列に変換
-		var bytes = [];
-		var char = 0;
-		for(var i =  0; i < str.length; i++) {
-			char = str.charCodeAt(i);
-			for(var j = 0; j < 8; j++) {
-				bytes.push((char & 0x80) >>> 7);
-				char = char << 1;
-			}
-		}
-
-		// 6bit で割り切れるようにビットを追加する
-		var surplus = bytes.length % 6;
-		for(var i = 0; i < 6 - surplus; i++) {
-			bytes.push(0);
-		}
-	
-		// 6bit ごとにグループ化
-		var groups = [];
-		for(var i = 0; i < bytes.length / 6; i++) {
-			groups[i] = parseInt(bytes.slice(i * 6, i * 6 + 6).join(''), 2);
-		}
 		
-		// 変換する
-		var result = "";
-		var Base64Table = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-			'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a',
-			'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-			'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4',
-			'5', '6', '7', '8', '9', '+', '/'];
-		for(var i = 0; i < groups.length; i++) {
-			result += Base64Table[groups[i]];
-		}
-		
-		// 4 で割り切れる文字数へ = を追加して調整する
-		var surplus = result.length % 4;
-		for(var i = 0; i < surplus; i++) {
-			result += '=';
-		}
-		
-		return result;
-	},
-	
 	test: function() {
 		var messageObject = {};
 		
